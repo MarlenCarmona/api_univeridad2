062fcdbbb4053a4bbc7bdcb3fe59b338d0c60f09
MarlenCarmona <1718110379@utectulancingo.edu.mx>
2020-02-27 19:40:42 -0600

    cambios

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #TODO buscar comandos de git en consola de visual, git con linea de comandos 
 #TODO subir archivos al repositorio desde la consola con los pasos que vienen, omitiendo el primero
+from web.template import ALLOWED_AST_NODES
+ALLOWED_AST_NODES.append('Constant')
 
 import web #from web.template import ALLOWED_AST_NODES
-#ALLOWED_AST_NODES.append('Constant')
+
 
 urls = (
     '/alumnos/?', 'application.controllers.alumnos.Alumnos' #el simbolo ? inidca que recibira variables en la URL
